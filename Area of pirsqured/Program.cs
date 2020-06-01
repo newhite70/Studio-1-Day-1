e78df68330e5d8956020b6f27204af9591f1ca9b
Initial Commit <newhite7070@gmail.com>
2020-06-01 15:51:56 -0400

    Add project files.

---
+++ Area of pirsqured/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Area_of_pirsqured
+{
+    class Program
+    {
+        static void Main()
+        {
+            double radius;
+            double areaofC;
+            double circofC;
+            double diaofC;
+            double gallonsUsed;
+            double mpg;
+
+            Console.WriteLine("What is the radius of the circle?");
+            string radiusinput = Console.ReadLine();
+            radius = double.Parse(radiusinput);
+
+            areaofC = Math.PI * (radius * radius);
+            circofC = 2 * Math.PI * radius;
+            diaofC = 2 * radius;
+
+            Console.WriteLine("How many miles per gallon does your car get?");
+            string mpgIn = Console.ReadLine();
+            mpg = double.Parse(mpgIn);
+            gallonsUsed = (circofC / mpg);
+
+            Console.WriteLine($"Your car would use {gallonsUsed} gallons of fuel to travel around this circle.");
+
+        }
+    }
+}
